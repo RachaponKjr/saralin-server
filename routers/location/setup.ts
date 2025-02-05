0bb80da23454d0ee0f52a258d521f9c9ba682320
RachaponKjr <rachapon.navy@gmail.com>
2025-02-05 14:53:27 +0700

    update location

--- routers/location/setup.ts
+++ routers/location/setup.ts
@@ -3,13 +3,13 @@ import Elysia, { t } from "elysia";
 const Location = t.Object({
   user_id: t.String(),
   address_line1: t.String(),
-  address_line2: t.Nullable(t.String()),
+  address_line2: t.Optional(t.String().nullable()),
   district: t.String(),
   city: t.String(),
   postal_code: t.String(),
-  latitude: t.Nullable(t.Number()),
-  is_default: t.Nullable(t.Boolean()),
-  longitude: t.Optional(t.Number()),
+  latitude: t.Optional(t.Number().nullable()),
+  is_default: t.Optional(t.Boolean()),
+  longitude: t.Optional(t.Number()).nullable(),
 });
 
 export const BodyReqLocation = new Elysia().model({
