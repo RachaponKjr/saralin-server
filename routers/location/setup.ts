940c8082b33b228ddf2d0f52b2946a1434a555a8
RachaponKjr <rachapon.navy@gmail.com>
2025-02-05 14:45:22 +0700

    update location

--- routers/location/setup.ts
+++ routers/location/setup.ts
@@ -3,12 +3,12 @@ import Elysia, { t } from "elysia";
 const Location = t.Object({
   user_id: t.String(),
   address_line1: t.String(),
-  address_line2: t.Optional(t.String()),
+  address_line2: t.Nullable(t.String()),
   district: t.String(),
   city: t.String(),
   postal_code: t.String(),
-  latitude: t.Optional(t.Number()),
-  is_default: t.Optional(t.Boolean()),
+  latitude: t.Nullable(t.Number()),
+  is_default: t.Nullable(t.Boolean()),
   longitude: t.Optional(t.Number()),
 });
 
